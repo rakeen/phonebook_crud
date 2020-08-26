e666af920097bf9fe318ae0d0413f8918ebfd58f
Shadman Rakeen <rakeen09@gmail.com>
2020-08-26 19:59:46 +0600

    Add Contacts Create test

--- tests/server.test.js
+++ tests/server.test.js
@@ -8,3 +8,19 @@ describe('server running', function () {
             .expect(200);
     });
 });
+
+const API_URL_PREFIX = '/api/v1';
+
+describe('Contact Create', function () {
+    it('should create new Contact', async function () {
+        const payload = {
+            name: "Lorem",
+            mobileNumber: "01722222222"
+        };
+        const response = await request(server)
+            .post(`${API_URL_PREFIX}/contacts`)
+            .send(payload)
+            .set('Accept', 'application/json')
+            .expect(201);
+    });
+});
