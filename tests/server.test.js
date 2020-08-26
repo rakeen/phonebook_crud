cd3e77918860d3a5dcf5b3649a48a17765be08cb
Shadman Rakeen <rakeen09@gmail.com>
2020-08-26 20:00:34 +0600

    Add Contacts Get test

--- tests/server.test.js
+++ tests/server.test.js
@@ -24,3 +24,22 @@ describe('Contact Create', function () {
             .expect(201);
     });
 });
+describe('Contact Get', function () {
+    it('should return contact details given contact id', async function () {
+        const contact = {
+            name: "Lorem 2",
+            mobileNumber: "01722422222"
+        };
+        const contactResponse = await request(server)
+            .post(`${API_URL_PREFIX}/contacts`)
+            .send(contact);
+        const contactId = contactResponse?.body?.contactId;
+
+        const response = await request(server)
+            .get(`${API_URL_PREFIX}/contacts?mobileNumber=${contact.mobileNumber}`)
+            .expect(200);
+
+        // mobileNumber search can return multiple results
+        expect(response.body[0]?.contactId).toBe(contactId);
+    });
+});
